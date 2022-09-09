9ba06a1367aa844791b71e7486c43fd79a1520c4
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-09-09 23:26:59 +0100

    work manager

--- app/src/main/java/com/lemzeeyyy/workmanagerdemo/MainActivity.java
+++ app/src/main/java/com/lemzeeyyy/workmanagerdemo/MainActivity.java
@@ -1,14 +1,26 @@
 package com.lemzeeyyy.workmanagerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        WorkRequest countWorkRequest = new OneTimeWorkRequest.Builder(DemoWorker.class)
+                .build();
+        btn.setOnClickListener(view -> {
+            WorkManager.getInstance(getApplicationContext()).enqueue(countWorkRequest);
+        });
     }
 }
